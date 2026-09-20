4fbbab8877d66acfe2f93e2551eec44f044dadc0
Héctor San Miguel <hector.sanmiguel@alumnos.uneatlantico.es>
2026-09-20 13:55:48 +0200

    Reto001 base finalizado

---
+++ entregas/sanmiguelHector/reto001/CentroComercial.java
@@ -0,0 +1,104 @@
+public class CentroComercial {
+
+    private Cola cola;
+    private Caja[] cajas;
+    private Tiempo tiempo;
+    private boolean llegaClienteEsteMinuto;
+    final private double PROBABILIDAD_LLEGADA = 0.6;
+    private Console console;
+
+    public CentroComercial() {
+        console = new Console();
+        cola = new Cola();
+        cajas = new Caja[5];
+        for (int i = 0; i < cajas.length; i++) {
+            cajas[i] = new Caja(i + 1);
+        }
+        cajas[0] = new CajaExpress(1);
+        cajas[4] = new CajaExpress(5);
+        tiempo = new Tiempo();
+    }
+
+    public void simular() {
+        do {
+            tiempo.avanzar();
+            this.procesarLlegadaCliente();
+            cola.registrarEstado();
+            this.asignarClientesACajas();
+            this.procesarAtencionCajas();
+            this.mostrarEstado();
+            this.pausar();
+        } while (!tiempo.haFinalizado());
+        this.mostrarResumen();
+    }
+
+    private void mostrarResumen() {
+        int minutosSinClientes = cola.obtenerMinutosSinClientes();
+        int personasEnCola = cola.obtenerCantidadPersonasEnCola();
+        int personasAtendidas = this.obtenerPersonasAtendidas();
+        int itemsVendidos = this.obtenerItemsVendidos();
+
+        console.writeln("Personas atendidas: " + personasAtendidas);
+        console.writeln("Personas en cola al cierre: " + personasEnCola);
+        console.writeln("Items vendidos: " + itemsVendidos);
+        console.writeln("Minutos sin clientes en cola: " + minutosSinClientes);
+    }
+
+    private int obtenerPersonasAtendidas(){
+        int totalItems=0;
+        for(int numeroCaja=0; numeroCaja<cajas.length; numeroCaja++){
+            totalItems= totalItems + cajas[numeroCaja].obtenerPersonasAtendidas();
+        }
+        return totalItems;
+    }
+
+    private int obtenerItemsVendidos(){
+        int totalItems=0;
+        for(int numeroCaja=0; numeroCaja<cajas.length; numeroCaja++){
+            totalItems = totalItems + cajas[numeroCaja].obtenerItemsVendidos();
+        }
+        return totalItems;
+    }
+
+    private void pausar() {
+        console.pause(1);
+    }
+
+    private void mostrarEstado() {
+        console.cleanScreen();
+        tiempo.mostrar(llegaClienteEsteMinuto);
+        cola.mostrar();
+        this.mostrarCajas();
+    }
+
+    private void mostrarCajas(){
+        for(int numeroCaja=0; numeroCaja<cajas.length; numeroCaja++){
+            cajas[numeroCaja].mostrar();
+        }
+    }
+
+    private void procesarAtencionCajas() {
+        for(int numeroCaja=0; numeroCaja<cajas.length; numeroCaja++){
+            cajas[numeroCaja].avanzarAtencion();
+        }
+    }
+
+    private void asignarClientesACajas() {
+        for(int numeroCaja=0; numeroCaja<cajas.length; numeroCaja++){
+            if (cajas[numeroCaja].estaLibre() 
+                && cola.hayClientes()
+                && cajas[numeroCaja].puedeAtender(cola.primero())){
+                Cliente cliente = cola.quitarCliente();
+                cajas[numeroCaja].asignar(cliente);
+            }
+        }
+    }
+
+    private void procesarLlegadaCliente() {
+        llegaClienteEsteMinuto = Math.random() <= PROBABILIDAD_LLEGADA;
+        if (llegaClienteEsteMinuto) {
+            Cliente cliente = new Cliente();
+            cola.añadirCliente(cliente);
+        }
+    }
+}
---
+++ entregas/sanmiguelHector/reto001/Tiempo.java
@@ -0,0 +1,44 @@
+public class Tiempo {
+
+    private final double HORA_APERTURA = 9.0;
+    private final double HORA_CIERRE = 13.0;
+    private final double MINUTO = 0.0167;
+    private double horaActual;
+    private Console console;
+
+    public Tiempo() {
+        horaActual = HORA_APERTURA;
+        console = new Console();
+    }
+
+    public void avanzar() {
+        horaActual = horaActual + MINUTO;
+    }
+
+    public boolean haFinalizado() {
+        return HORA_CIERRE < horaActual;
+    }
+
+    public void mostrar(boolean llegaClienteEsteMinuto) {
+        console.write(this.horaHumana());
+        console.write(" ");
+        console.writeln((llegaClienteEsteMinuto ? "" : "no") + " llegó un cliente");
+    }
+
+    private String horaHumana() {
+        int hora = (int) horaActual;
+        int minutos =(int) ((horaActual - hora)*60);
+        return hora + ":" + minutos;
+    }
+
+    public static void main(String[] args) {
+        Tiempo tiempo = new Tiempo();
+        tiempo.mostrar(true);
+        for(int i=0;i<60;i++){
+            tiempo.avanzar();
+        }
+        tiempo.mostrar(true);
+    }
+
+
+}
